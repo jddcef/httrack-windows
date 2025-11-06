fdaf034e18c888c2777c44057f02dcba6ece9c27
David de Caires e Freitas <jddcef@gmail.com>
2025-11-06 20:44:34 +0200

    refactor(build): Remove Serializable attribute from OurMSBuildSettings
    - Remove [Serializable] attribute from OurMSBuildSettings class
    - Simplify class definition by removing unnecessary serialization marker
    - Maintain compatibility with existing MSBuildSettings implementation

--- build/NukePlusPlus/OurMSBuildSettings.cs
+++ build/NukePlusPlus/OurMSBuildSettings.cs
@@ -8,7 +8,6 @@ using System.Diagnostics.CodeAnalysis;
 namespace NukePlusPlus {
 	[PublicAPI]
 	[ExcludeFromCodeCoverage]
-	[Serializable]
 	public class OurMSBuildSettings : MSBuildSettings {
 		protected override Arguments ConfigureProcessArguments(Arguments arguments) {
 			var args = base.ConfigureProcessArguments(arguments);
